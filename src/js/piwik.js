e31f39b0ea2d20a1dba0a447b5d9da88a2a477d2
Léo Lam <leo@innovatetechnologi.es>
2014-07-17 16:55:39 +0200

    Move Piwik tracking code into separate file
    This allows for the code to be minified, optimised and combined.

---
+++ src/js/piwik.js
@@ -0,0 +1,16 @@
+/* global document */
+var _paq = _paq || [];
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(["setCookieDomain", "*.shoutca.st"]);
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+_paq.push(["setTrackerUrl", "https://piwik.shoutca.st/piwik.php"]);
+_paq.push(["setSiteId", "1"]);
+var b = document,
+c = b.createElement("script"),
+d = b.getElementsByTagName("script")[0];
+c.type = "text/javascript";
+c.defer = !0;
+c.async = !0;
+c.src = "https://piwik.shoutca.st/piwik.js";
+d.parentNode.insertBefore(c, d);
