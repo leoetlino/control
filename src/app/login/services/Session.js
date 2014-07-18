918afd9aacbadc0eceb651c044a052f45821a0d5
Léo Lam <leo@innovatetechnologi.es>
2014-07-18 20:11:36 +0200

    Directly return Session methods
    Why bother declaring them with this.methodName every time?

--- src/app/login/services/Session.js
+++ src/app/login/services/Session.js
@@ -1,26 +1,27 @@
 define(['control'], function (control) {
     control.service('Session', function (USER_ROLES, localStorageService) {
-        this.create = function (token) {
-            this.token = token;
-            this.userRole = [USER_ROLES.user];
-            localStorageService.set('token', token);
-        };
+        return {
+            create: function (token) {
+                this.token = token;
+                this.userRole = [USER_ROLES.user];
+                localStorageService.set('token', token);
+            },
 
-        this.update = function (token) {
-            this.destroy();
-            this.create(token);
-        };
+            update: function (token) {
+                this.destroy();
+                this.create(token);
+            },
 
-        this.createFromLocalStorage = function () {
-            this.token = localStorageService.get('token');
-            this.userRole = [USER_ROLES.user];
-        };
+            createFromLocalStorage: function () {
+                this.token = localStorageService.get('token');
+                this.userRole = [USER_ROLES.user];
+            },
 
-        this.destroy = function () {
-            this.token = null;
-            this.userRole = null;
-            localStorageService.remove('token');
+            destroy: function () {
+                this.token = null;
+                this.userRole = null;
+                localStorageService.remove('token');
+            }
         };
-        return this;
     });
 });
