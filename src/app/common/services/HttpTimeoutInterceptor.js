98e288299334475baabba5b8e92922c5b35755d1
Léo Lam <leo@innovatetechnologi.es>
2014-07-18 14:37:22 +0200

    Show an alert when request times out

--- src/app/common/services/HttpTimeoutInterceptor.js
+++ src/app/common/services/HttpTimeoutInterceptor.js
@@ -1,10 +1,16 @@
 /* global define */
 define(['control'], function (control) {
-    control.factory('HttpTimeoutInterceptor', function () {
+    control.factory('HttpTimeoutInterceptor', function ($q, $ngBootbox) {
         return {
             'request': function(config) {
                 config.timeout = 10000;
                 return config;
+            },
+            responseError: function (response) {
+                if (response.status === -1) {
+                    $ngBootbox.alert('Control is currently unavailable. Please try again later. If this occurs again, let us know as soon as possible.');
+                }
+                return $q.reject(response);
             }
         };
     });
