8c33925742ed1b523e612fe3f5d264bd8f81e872
Léo Lam <leo@innovatetechnologi.es>
2014-07-18 14:06:58 +0200

    Redirect back to / if user is already logged in
    Redirect back to the home page route (/) if the user is already logged
    in and requests /login.

--- src/app/login/services/AuthService.js
+++ src/app/login/services/AuthService.js
@@ -1,15 +1,20 @@
 define(['control'], function (control) {
-    control.factory('AuthService', function ($http, Session, USER_ROLES, localStorageService, ENV) {
+    control.factory('AuthService', function ($http, Session, USER_ROLES, localStorageService, ENV, $interval) {
         return {
             login: function (credentials) {
                 return $http
                     .post('https://' + ENV.apiEndpoint + '/authenticate', credentials)
                     .then(function (res) {
                         Session.create(res.data.token);
+                        this.keepAlivePromise = $interval(this.keepAlive, 1000 * 60 * 25);
                     });
             },
             logout: function () {
                 Session.destroy();
+                if (angular.isDefined(this.keepAlivePromise)) {
+                    $interval.cancel(this.keepAlivePromise);
+                    this.keepAlivePromise = null;
+                }
             },
             isAuthenticated: function () {
                 if (localStorageService.get('token') !== 'undefined' && localStorageService.get('token') !== null) {
@@ -29,6 +34,10 @@ define(['control'], function (control) {
                 }
                 return (this.isAuthenticated() &&
                     authorizedRoles.indexOf(Session.userRole) !== -1);
+            },
+            keepAlivePromise: null,
+            keepAlive: function () {
+
             }
         };
     });
