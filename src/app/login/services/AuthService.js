a6edbe0a1041afe445db553636bb08ae18168a53
Léo Lam <leo@innovatetechnologi.es>
2014-07-18 14:06:58 +0200

    Send a keep-alive request every 25 minutes
    Send a keep-alive request to the server every 25 minutes (5 minutes
    before a token expires) to request a new token to use.
    
    Note that the token should be invalidated and added to a blacklist once
    it is invalidated. Protected routes should return 401 if the token is
    expired. I'll probably implement this in a future commit to ITFrame.

--- src/app/login/services/AuthService.js
+++ src/app/login/services/AuthService.js
@@ -1,15 +1,20 @@
 define(['control'], function (control) {
-    control.factory('AuthService', function ($http, Session, USER_ROLES, localStorageService, ENV) {
+    control.factory('AuthService', function ($http, Session, USER_ROLES, localStorageService, ENV, $interval) {
         return {
             login: function (credentials) {
                 return $http
                     .post('https://' + ENV.apiEndpoint + '/authenticate', credentials)
                     .then(function (res) {
                         Session.create(res.data.token);
+                        this.keepAlivePromise = $interval(this.keepAlive, 1000 * 60 * 25);
                     });
             },
             logout: function () {
                 Session.destroy();
+                if (angular.isDefined(this.keepAlivePromise)) {
+                    $interval.cancel(this.keepAlivePromise);
+                    this.keepAlivePromise = null;
+                }
             },
             isAuthenticated: function () {
                 if (localStorageService.get('token') !== 'undefined' && localStorageService.get('token') !== null) {
@@ -43,6 +48,17 @@ define(['control'], function (control) {
                 }
                 return (this.isAuthenticated() &&
                     authorizedRoles.indexOf(Session.userRole) !== -1);
+            },
+            keepAlivePromise: null,
+            keepAlive: function () {
+                if (!this.isReallyAuthenticated()) {
+                    return;
+                }
+
+                $http.post('https://' + ENV.apiEndpoint + '/control/keep-alive')
+                .then(function (res) {
+                    Session.update(res.data.token);
+                });
             }
         };
     });
