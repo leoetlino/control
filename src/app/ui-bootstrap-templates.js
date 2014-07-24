bf81848d2c990aa76b81888e6d2c736479451158
Léo Lam <leo@innovatetechnologi.es>
2014-07-24 17:28:49 +0200

    Update generated templates file

--- src/app/ui-bootstrap-templates.js
+++ src/app/ui-bootstrap-templates.js
@@ -2,5 +2,5 @@ angular.module('templates-ngBootstrapTemplates', ['template/tabs/tabset.html']);
 
 angular.module("template/tabs/tabset.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/tabs/tabset.html",
-    "<div class=row><div ng-class=\"{'col-sm-4': vertical}\"><ul class=\"nav nav-{{type || 'tabs'}}\" ng-class=\"{'nav-stacked': vertical, 'nav-justified': justified}\" ng-transclude></ul></div><div ng-class=\"{'col-sm-8': vertical}\"><div class=tab-content><div class=tab-pane ng-repeat=\"tab in tabs\" ng-class=\"{active: tab.active}\" tab-content-transclude=tab></div></div></div></div>");
+    "<div class=row> <div ng-class=\"{'col-sm-4': vertical}\"> <ul class=\"nav nav-{{type || 'tabs'}}\" ng-class=\"{'nav-stacked': vertical, 'nav-justified': justified}\" ng-transclude></ul> </div> <div ng-class=\"{'col-sm-8': vertical}\"> <div class=tab-content> <div class=tab-pane ng-repeat=\"tab in tabs\" ng-class=\"{active: tab.active}\" tab-content-transclude=tab> </div> </div> </div> </div>");
 }]);
