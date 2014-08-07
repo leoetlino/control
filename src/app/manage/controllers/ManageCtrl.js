1a8707f6b02ef40c59992bc5345e5078cb02a7bb
Léo Lam <leo@innovatetechnologi.es>
2014-08-07 10:41:06 +0200

    Remove duplicate code
    Move some of the code into helper functions provided by the controller
    in the $scope.

--- src/app/manage/controllers/ManageCtrl.js
+++ src/app/manage/controllers/ManageCtrl.js
@@ -1,5 +1,11 @@
 define(['control'], function (control) {
     control.controller('ManageCtrl', function ($scope, services) {
         $scope.products = services;
+        $scope.isInactive = function (product) {
+            return (['Terminated', 'Suspended', 'Cancelled', 'Pending'].indexOf(product.status) > -1);
+        };
+        $scope.isStreamingServer = function (product) {
+            return (product.group.toLowerCase().indexOf('cast') === -1);
+        };
     });
 });
