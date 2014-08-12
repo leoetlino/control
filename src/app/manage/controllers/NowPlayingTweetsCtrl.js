a90d4b35d3086bc394f33b931808de26c7a8e42a
Léo Lam <leo@innovatetechnologi.es>
2014-08-12 21:56:42 +0200

    Set the switch to false on #NowPlaying remove

--- src/app/manage/controllers/NowPlayingTweetsCtrl.js
+++ src/app/manage/controllers/NowPlayingTweetsCtrl.js
@@ -109,6 +109,9 @@ define(['control'], function (control) {
             $scope.submitting = true;
             NowPlayingTweetsService.removeSettings($scope.service.username).then(function () {
                 $scope.submitting = false;
+                unregisterWatch();
+                $scope.nowPlayingState = false;
+                watchNowPlayingSwitch();
                 flash.to('alert-now-playing-box').success = 'Your #NowPlaying settings have been removed.';
             }, function () {
                 flash.to('alert-now-playing-box').error = 'Something went wrong while removing your settings. Your settings were not removed. Please try again.';
