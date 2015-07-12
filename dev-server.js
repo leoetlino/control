6ca6c20e727a739f7e81acc2cb5d18f0f10f0254
Léo Lam <leo@innovatetechnologi.es>
2015-07-12 18:14:13 +0800

    Dev server: Serve 404 if requesting .html or .json file

--- dev-server.js
+++ dev-server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static('dev/'));
 
 app.get('/*', function (req, res) {
-    if (req.originalUrl.indexOf('.css') !== -1 || req.originalUrl.indexOf('.js') !== -1) {
+    if (req.originalUrl.indexOf('.css') !== -1 || req.originalUrl.indexOf('.js') !== -1 || req.originalUrl.indexOf('.html') !== -1 || req.originalUrl.indexOf('.json') !== -1) {
         return res.status(404).send('Not found');
     }
     res.sendFile(path.join(__dirname, 'dev/index.html'));
