c25224b5cb4225c5d0484d8802d72ff34ab00fc2
Léo Lam <leo@innovatetechnologi.es>
2015-07-14 17:22:54 +0800

    LoginCtrl: Split constants into a separate file

---
+++ src/app/login/constants.js
@@ -0,0 +1,17 @@
+control.constant('AUTH_EVENTS', {
+    loginSuccess: 'auth-login-success',
+    loginFailed: 'auth-login-failed',
+    logoutSuccess: 'auth-logout-success',
+    logoutFailed: 'auth-logout-failed',
+    sessionTimeout: 'auth-session-timeout',
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized',
+    badRequest: 'auth-bad-request'
+});
+
+control.constant('USER_ROLES', {
+    all: '*',
+    admin: 'admin',
+    user: 'user',
+    'public': 'public'
+});
