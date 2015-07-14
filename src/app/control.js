c150f99f0ff8e8a851d9c1824255bf44705b8f0e
Léo Lam <leo@innovatetechnologi.es>
2015-07-14 17:11:25 +0800

    Remove angular-flash

--- src/app/control.js
+++ src/app/control.js
@@ -20,8 +20,6 @@ control = null;
         'ngSanitize',
         'LocalStorageModule',
         'angular-loading-bar',
-        'angular-flash.service',
-        'angular-flash.flash-alert-directive',
         'ngFileUpload',
         'colorpicker.module',
         'toggle-switch',
@@ -30,12 +28,7 @@ control = null;
         'picardy.fontawesome'
     ]);
 
-    control.config(function ($routeSegmentProvider, $routeProvider, $locationProvider, $controllerProvider, $compileProvider, $filterProvider, USER_ROLES, flashProvider, $httpProvider, $provide, $alertProvider) {
-
-        flashProvider.errorClassnames.push('alert-danger');
-        flashProvider.warnClassnames.push('alert-warning');
-        flashProvider.infoClassnames.push('alert-info');
-        flashProvider.successClassnames.push('alert-success');
+    control.config(function ($routeSegmentProvider, $routeProvider, $locationProvider, $controllerProvider, $compileProvider, $filterProvider, USER_ROLES, $httpProvider, $provide, $alertProvider) {
 
         angular.extend($alertProvider.defaults, {
             animation: 'am-fade-and-slide-top',
