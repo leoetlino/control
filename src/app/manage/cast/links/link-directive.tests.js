cdb5b257545d7ef376873c0405376196e32b8a89
Léo Lam <leo@innovatetechnologi.es>
2015-08-05 22:54:00 +0800

    Add tests for the link directive

---
+++ src/app/manage/cast/links/link-directive.tests.js
@@ -0,0 +1,22 @@
+/* global describe, module, inject, it, expect, beforeEach, spyOn, jasmine */
+
+describe('link directive', function () {
+
+    var compiledElement;
+    var template = '<p><a link="http://example.com"></a></p>';
+
+    beforeEach(module('control'));
+    beforeEach(inject(function ($rootScope, $compile) {
+        this.scope = $rootScope.$new();
+        compiledElement = $compile(template)(this.scope);
+        this.scope.$digest();
+    }));
+
+    it('should remove the protocol from the label', function () {
+        expect(compiledElement.children('a').text()).toBe('example.com');
+    });
+
+    it('should force HTTPS on the href attribute', function () {
+        expect(compiledElement.children('a').attr('href')).toBe('https://example.com');
+    });
+});
