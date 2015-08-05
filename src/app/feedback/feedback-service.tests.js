a2347136d4f2c443a35cf5fd9b2404ffe262588a
Léo Lam <leo@innovatetechnologi.es>
2015-08-05 17:29:10 +0800

    Add tests for FeedbackService

---
+++ src/app/feedback/feedback-service.tests.js
@@ -0,0 +1,62 @@
+/* global describe, module, inject, it, expect, beforeEach, jasmine */
+
+describe('FeedbackService', function () {
+
+    beforeEach(module('control'));
+    beforeEach(module(function ($provide) {
+        $provide.value('ENV', { apiEndpoint: 'itframe.shoutca.st' });
+    }));
+    beforeEach(inject(function (FeedbackService, $httpBackend) {
+        this.service = FeedbackService;
+        this.$httpBackend = $httpBackend;
+    }));
+
+    describe('the service', function () {
+        it('should provide a send method', function () {
+            expect(this.service.send).toEqual(jasmine.any(Function));
+        });
+    });
+
+    describe('the send() method', function () {
+
+        it('should succeed if everything is OK', function () {
+            var succeeded = false;
+
+            var title = 'dummy';
+            var message = 'dummy message';
+
+            this.$httpBackend.expectPOST('https://itframe.shoutca.st/control/feedback/', {
+                title: title,
+                message: message,
+            }).respond(200, { result: 'success' });
+
+            this.service.send(title, message).then(function () {
+                succeeded = true;
+            });
+
+            this.$httpBackend.flush();
+            expect(succeeded).toBe(true);
+        });
+
+        it('should fail if the server failed', function () {
+            var succeeded = false;
+
+            var title = 'dummy';
+            var message = 'dummy message';
+
+            this.$httpBackend.expectPOST('https://itframe.shoutca.st/control/feedback/', {
+                title: title,
+                message: message,
+            }).respond(500, { result: 'error' });
+
+            this.service.send(title, message).then(function () {
+                succeeded = true;
+            });
+
+            this.$httpBackend.flush();
+            expect(succeeded).not.toBe(true);
+        });
+
+    });
+
+});
