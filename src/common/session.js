715c40a96e21ecbf7187d01d8569d1c66818560d
Léo Lam <leo@innovatetechnologi.es>
2016-01-13 15:45:27 +0100

    Fix tests
    Breaking change in Babel 6.4.0.

--- src/common/session.js
+++ src/common/session.js
@@ -9,22 +9,22 @@ export default class Session {
     this.userRole = [this.USER_ROLES.user];
     this.localStorageService.set("token", token);
     this.$rootScope.$broadcast("sessionCreated");
-  }
+  };
 
   update = (token) => {
     this.token = token;
     this.localStorageService.set("token", token);
     this.$rootScope.$broadcast("sessionUpdated");
-  }
+  };
 
   createFromLocalStorage = () => {
     this.create(this.localStorageService.get("token"));
-  }
+  };
 
   destroy = () => {
     this.token = null;
     this.userRole = null;
     this.localStorageService.remove("token");
     this.$rootScope.$broadcast("sessionDestroyed");
-  }
+  };
 }
