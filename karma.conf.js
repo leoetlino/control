892b51d69d950a8624dc6b9915103126454f2c91
Léo Lam <leo@innovatetechnologi.es>
2016-01-23 14:26:30 +0100

    Fix formatting in meta project JS files
    This fixes formatting in meta project files such as webpack build files
    and karma's configuration file.

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,27 @@
 module.exports = function (config) {
-    config.set({
-        files: [
-            'src/tests.webpack.js',
-        ],
-        preprocessors: {
-            'src/tests.webpack.js': ['webpack', 'sourcemap'],
-        },
-        colors: true,
-        logLevel: config.LOG_INFO,
-        reporters: ['mocha'],
-        plugins: [
-            require('karma-phantomjs-launcher'),
-            require('karma-jasmine'),
-            require('karma-mocha-reporter'),
-            require('karma-webpack'),
-            require('karma-sourcemap-loader'),
-        ],
-        frameworks: ['jasmine'],
-        browsers: ['PhantomJS'],
-        singleRun: true,
-        webpack: require('./webpack.make')({ BUILD: false, TEST: true }),
-        webpackMiddleware: {
-            noInfo: true,
-        },
-    });
+  config.set({
+    files: [
+      "src/tests.webpack.js",
+    ],
+    preprocessors: {
+      "src/tests.webpack.js": ["webpack", "sourcemap"],
+    },
+    colors: true,
+    logLevel: config.LOG_INFO,
+    reporters: ["mocha"],
+    plugins: [
+      require("karma-phantomjs-launcher"),
+      require("karma-jasmine"),
+      require("karma-mocha-reporter"),
+      require("karma-webpack"),
+      require("karma-sourcemap-loader"),
+    ],
+    frameworks: ["jasmine"],
+    browsers: ["PhantomJS"],
+    singleRun: true,
+    webpack: require("./webpack.make")({ BUILD: false, TEST: true }),
+    webpackMiddleware: {
+      noInfo: true,
+    },
+  });
 };
