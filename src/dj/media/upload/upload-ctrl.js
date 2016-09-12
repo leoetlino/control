bf95dda3e58ebf7df00a0388651e58a66af46548
Maartje Eyskens <meyskens@me.com>
2016-09-12 15:48:11 +0200

    Add file filter

--- src/dj/media/upload/upload-ctrl.js
+++ src/dj/media/upload/upload-ctrl.js
@@ -9,5 +9,14 @@ export default /*@ngInject*/ function ($scope, FileUploader, ENV, localStorageSe
     headers: {
       Authorization: "Bearer " + localStorageService.get("sessionData").token,
     },
+    filters: [
+      {
+        name: "songFilter",
+        fn: function (item) {
+          var type = "|" + item.type.slice(item.type.lastIndexOf("/") + 1) + "|";
+          return "|mp3|mpeg|mp4|m4a|x-m4a|ogg|flac|wav|amr|".indexOf(type) !== -1;
+        },
+      },
+    ],
   });
 }
