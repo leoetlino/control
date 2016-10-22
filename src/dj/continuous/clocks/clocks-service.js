d90d6f1167da3b2773e05c947b3b8831e8a7cf39
Maartje Eyskens <meyskens@me.com>
2016-10-22 22:23:28 +0200

    Create clocks service

--- src/dj/continuous/clocks/clocks-service.js
+++ src/dj/continuous/clocks/clocks-service.js
@@ -2,17 +2,11 @@ export default class ClocksService {
     /*@ngInject*/
   constructor($http, $rootScope, ENV) {
     const username = $rootScope.service.username;
-    this.getTags = () => {
-      return $http.get(`${ENV.apiEndpoint}/control/cast/dj/tags/${username}`).then(resp => resp.data);
+    this.getClocks = () => {
+      return $http.get(`${ENV.apiEndpoint}/control/cast/dj/clocks/${username}`).then(resp => resp.data);
     };
-    this.deteleTag = (id) => {
-      return $http.delete(`${ENV.apiEndpoint}/control/cast/dj/tags/${username}/${id}`).then(resp => resp.data);
-    };
-    this.addTag = (tag) => {
-      return $http.put(`${ENV.apiEndpoint}/control/cast/dj/tags/${username}`, tag).then(resp => resp.data);
-    };
-    this.updateTag = (tag) => {
-      return $http.post(`${ENV.apiEndpoint}/control/cast/dj/tags/${username}/${tag._id}`, tag).then(resp => resp.data);
+    this.setClocks = (clocks) => {
+      return $http.put(`${ENV.apiEndpoint}/control/cast/dj/clocks/${username}`, clocks).then(resp => resp.data);
     };
   }
 }
