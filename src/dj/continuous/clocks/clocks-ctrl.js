ca9e722e9e3b233cac9437100bbcbdc6e7838db8
Maartje Eyskens <meyskens@me.com>
2016-10-29 16:50:37 +0200

    Make clocks save

--- src/dj/continuous/clocks/clocks-ctrl.js
+++ src/dj/continuous/clocks/clocks-ctrl.js
@@ -1,4 +1,6 @@
-export default /*@ngInject*/ function ClocksCtrl(ClocksService, ClocksColorService, $modal, $scope) {
+import { angular, lodash as _ } from "../../../vendor";
+
+export default /*@ngInject*/ function ClocksCtrl(ClocksService, ClocksColorService, TagsService, $modal, $scope) {
   this.daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
   this.daysOfWeekSelect = [
       { "value": 1, "label": "Monday" },
@@ -11,13 +13,24 @@ export default /*@ngInject*/ function ClocksCtrl(ClocksService, ClocksColorServi
   ];
   this.hoursOfDay = [];
   this.clocks = [];
+  this.tags = [];
+  this.saving = false;
+
   for (let i = 0; i <= 23; i++) {
     this.hoursOfDay.push(i);
   }
   this.ClocksColorService = ClocksColorService; // forward to the view
 
-  ClocksService.getClocks().then((clocks) => {
-    this.clocks = clocks;
+
+  this.loadClocks = () => {
+    return ClocksService.getClocks().then((clocks) => {
+      this.clocks = clocks;
+      this.originalClocks = angular.copy(clocks);
+    });
+  };
+
+  TagsService.getTags().then((tags) => {
+    this.tags = tags;
   });
 
   this.getClockForTime = (day, hour) => {
@@ -31,11 +44,16 @@ export default /*@ngInject*/ function ClocksCtrl(ClocksService, ClocksColorServi
     return false;
   };
 
+  this.getTagForId = (_id) => {
+    return _. filter(this.tags, { _id })[0];
+  };
+
   this.addClock = () => {
     this.newClock = {
       name: "",
       color: "#FFFFFF",
       tags: [],
+      tagTime: [100],
       start: {
         dayOfWeek: 1,
         hour: 0,
@@ -51,8 +69,49 @@ export default /*@ngInject*/ function ClocksCtrl(ClocksService, ClocksColorServi
   };
 
   this.pushClock = (clock) => {
-    // validation stuff
+    // validation
+
+
+    // set correct data model
+    const tags = angular.copy(clock.tags);
+    const tagTime = angular.copy(clock.tagTime);
+    delete clock.tagTime;
+    clock.tags = [];
+    for (let i = 0; i < tags.length; i++) {
+      clock.tags.push({
+        percent: tagTime[i],
+        tag: tags[i]._id,
+      });
+    }
+
+    // push clock
     this.clocks.push(clock);
     return true;
+  };
+
+  this.canBeSaved = () => {
+    let canBeSaved = true;
+    // validation stuff
+    if (this.clocks.length === 0) {
+      canBeSaved = false;
+    }
+
+    if (this.saving) {
+      canBeSaved = false;
+    }
+    // check on changes
+    if (this.originalClocks.toString() === this.clocks.toString()) {
+      canBeSaved = false;
+    }
+    return canBeSaved;
+  };
+
+  this.save = () => {
+    this.saving = true;
+    ClocksService.setClocks(this.clocks).then(() => {
+      this.loadClocks().then(() => {
+        this.saving = false;
+      });
+    });
   };
 }
--- src/dj/media/tags/index.js
+++ src/dj/media/tags/index.js
@@ -2,10 +2,12 @@ import { angular } from "../../../vendor";
 
 import TagsCtrl from "./tags-ctrl";
 import TagsService from "./tags-service";
+import TagsColorService from "./tags-color-service";
 
 export default angular.module("control.dj.media.tags", [])
     .controller("TagsCtrl", TagsCtrl)
     .service("TagsService", TagsService)
+    .service("TagsColorService", TagsColorService)
     .run(/*@ngInject*/ (DjManageService) => {
       DjManageService.addItem({
         sectionId: "media",
