4a893754c9ee7f0645698a9b5592aa7cac8ded54
Maartje Eyskens <meyskens@me.com>
2016-12-31 16:30:22 +0100

    add Go to Centova Cast button

--- src/manage/index.js
+++ src/manage/index.js
@@ -19,32 +19,40 @@ export default angular.module("control.manage", [
   manageCast,
   manageExtra,
 ])
-    .service("ServicesService", ServicesService)
-    .service("ManageService", ManageService)
-    .controller("ManageCtrl", ManageCtrl)
-    .config(/*@ngInject*/ ($routeSegmentProvider, $routeProvider, USER_ROLES) => {
-      $routeSegmentProvider
-            .when("/manage", "manage")
-            .when("/manage/information", "manage.information")
-            .segment("manage", {
-              template: require("./manage.html"),
-              authorizedRoles: [USER_ROLES.all],
-              title: "Manage your servers",
-              resolve: {
-                service: ["ServicesService", (SService) => SService.initAndGetService()],
-              },
-              controller: "ManageCtrl",
-              watcher: watchForService,
-            })
-            .within().segment("information", {
-              default: true,
-              template: require("./information-pane.html"),
-              authorizedRoles: [USER_ROLES.all],
-              title: "Information",
-              watcher: watchForService,
-            })
-            .up()
-            .otherwise("/");
-      $routeProvider.when("/manage/request-app", { redirectTo: "/manage/apps" });
-    })
-    .name;
+  .service("ServicesService", ServicesService)
+  .service("ManageService", ManageService)
+  .controller("ManageCtrl", ManageCtrl)
+  .config(/*@ngInject*/($sceDelegateProvider) => {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      // Allow same origin resource loads.
+      "self",
+      // Allow loading from our assets domain.  Notice the difference between * and **.
+      "https://*.shoutca.st/**",
+    ]);
+  })
+  .config(/*@ngInject*/($routeSegmentProvider, $routeProvider, USER_ROLES) => {
+    $routeSegmentProvider
+      .when("/manage", "manage")
+      .when("/manage/information", "manage.information")
+      .segment("manage", {
+        template: require("./manage.html"),
+        authorizedRoles: [USER_ROLES.all],
+        title: "Manage your servers",
+        resolve: {
+          service: ["ServicesService", (SService) => SService.initAndGetService()],
+        },
+        controller: "ManageCtrl",
+        watcher: watchForService,
+      })
+      .within().segment("information", {
+        default: true,
+        template: require("./information-pane.html"),
+        authorizedRoles: [USER_ROLES.all],
+        title: "Information",
+        watcher: watchForService,
+      })
+      .up()
+      .otherwise("/");
+    $routeProvider.when("/manage/request-app", { redirectTo: "/manage/apps" });
+  })
+  .name;
